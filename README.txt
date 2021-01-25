e584de08ad723e2c7a62b7a3aef363ef55655a1c
Rafael Munoz <rafael.munoz77@gmail.com>
2021-01-25 22:06:13 +0100

    v 16 - Redirijo los eventos de movimiento del ratón desde el Canvas hasta el jugador

--- README.txt
+++ README.txt
@@ -64,3 +64,7 @@ v.14 - Convertimos la clase SpaceInvaders a un Singleton.
 
 v.15 - Aprovechamos el Singleton para conseguir que los monstruos reboten correctamente, al  tener
 estos acceso a las medidas reales que ocupa el Canvas.
+
+v.16 - Redirijo los eventos de ratón desde el Canvas hasta el jugador.
+- Utilizo la sobrescritura del método "mouseMoved()" de un MouseAdapter para controlar el evento
+de que el ratón se mueva sobre el Canvas. Redirijo dicho evento hacia un método en el jugador.
--- src/org/rmrsoft/spaceInvaders/Player.java
+++ src/org/rmrsoft/spaceInvaders/Player.java
@@ -49,6 +49,27 @@ public class Player extends Actor {
 	
 	}
 
+	/**
+	 * Mediante la llamada a este método, podemos cambiar la posición del jugador a unas
+	 * nuevas coordenadas
+	 * @param x
+	 * @param y
+	 */
+	public void mover(int x, int y) {
+		this.x = x;
+		this.y = y;
+		// Controlo los casos en los que el jugador pueda salir del Canvas
+		MiCanvas canvas = SpaceInvaders.getInstance().getCanvas(); // Referencia al objeto Canvas usado
+
+		// Compruebo si el ratón sale por la derecha
+		if (this.x > (canvas.getWidth() - this.ancho)) {
+			this.x = canvas.getWidth() - this.ancho;
+		}
+		// Compruebo si el ratón sale por abajo
+		if (this.y > (canvas.getHeight() - this.alto)) {
+			this.y = canvas.getHeight() - this.alto;
+		}
+	}
 
 	// Getters y Setters
 	
